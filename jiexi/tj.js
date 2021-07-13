24e9f1135b546d87be5f4188acb34391392f1897
shuchangqzone <shuchangqzone@gmail.com>
2021-07-13 14:13:42 +0800

    Add files via upload

---
+++ jiexi/tj.js
@@ -0,0 +1,3 @@
+if(window.console&&window.console.log){
+console.log("%c%c桃花源%c爱了爱了", "line-height:26px;", "line-height:16px;padding:2px 6px;border-radius: 5px 0px 0px 5px;background:#35495e;color:#fff;font-size:12px;", "padding:2px 8px;background:#0093ff;color:#fff;line-height:16px;font-size:12px;border-radius: 0px 5px 5px 0px;");
+}
